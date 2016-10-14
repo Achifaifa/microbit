281446ed27c99849054fa50a82cde24b4d384bf2
Yuri Numerov <achi@hush.ai>
2016-10-14 15:09:06 +0200

    final badge version

--- badge.py
+++ badge.py
@@ -1,11 +1,13 @@
 import microbit
 
 while 1:
-  microbit.display.show("GAMAKER", delay=200, wait=True, loop=False, clear=False)
-  microbit.display.scroll(" LEMA AQUI", delay=80)
+  microbit.display.scroll("PKT TEAM", delay=80)
   for i in range(3):
     microbit.display.show(microbit.Image.HEART)
     microbit.sleep(500)
     microbit.display.show(microbit.Image.HEART_SMALL)
     microbit.sleep(500)
-  microbit.display.scroll("<--", delay=80)
+  microbit.display.show("ACHIFAIFA", delay=200)
+  for i in range(3):
+    microbit.display.show(microbit.Image.SILLY)
+    microbit.sleep(1000)
